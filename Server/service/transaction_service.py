9b4632d78fb570ca277c87098925b88c5aff1100
amit ben basat <amitbasat124@gmail.com>
2022-11-14 16:21:37 +0200

    adding the service for transaction and catgories as the route of each

--- Server/service/transaction_service.py
+++ Server/service/transaction_service.py
@@ -1,5 +1,13 @@
 
-from Models import transactions_model
 from repositories import transactions_repository
-def get_transactions():
 
+
+def get_transactions():
+    return transactions_repository.get_transactions_query();
+
+def add_transaction(transaction):
+    return transactions_repository.add_transaction_query(transaction)
+
+
+def delete_transaction(id_transaction):
+    return transactions_repository.delete_transaction(id_transaction)
