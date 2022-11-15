b28fb7ea4a6c7ab2ee81b2b6da72fbdfbd046ffd
amit ben basat <amitbasat124@gmail.com>
2022-11-15 18:37:46 +0200

    adding transaction with the transactions component

--- Client/src/components/transiction.js
+++ Client/src/components/transiction.js
@@ -1,8 +1,24 @@
-
-
-export default function Transiction() {
-
+import React, { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { StyledTableCell,StyledTableRow } from "../Style/transactionStyle";
+export default function Transiction(props) {
+    const [transaction]=useState(props.transaction)
     return (
-       <div></div>
+        <StyledTableRow key={transaction.transaction_id}>
+        <StyledTableCell component="th" scope="row">
+          {transaction.transaction_vendor}
+        </StyledTableCell>
+        <StyledTableCell align="left">{transaction.transiction_amount}</StyledTableCell>
+        <StyledTableCell align="left">{transaction.category_name}</StyledTableCell>
+       <StyledTableCell>
+        <Box textAlign='left'>
+          <Button justify="left" variant="contained" color="error">
+             delete
+          </Button>
+        </Box>
+      </StyledTableCell>
+       
+      </StyledTableRow>
     )
 }
--- Client/src/components/transictions.js
+++ Client/src/components/transictions.js
@@ -1,43 +1,17 @@
-import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
+import Transaction from "./Transiction";
 import { fetchTransaction } from "../ApiClient";
-
+import { StyledTableCell,StyledTableRow } from "../Style/transactionStyle";
 import React, { useState, useEffect } from 'react'
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
 
 
 
-
-
-
-
-export default function Transictions(props) {
+export default function Transictions() {
     const [Transactions,setTransactions]=useState([])
     const [error, setError] = useState(null);
     useEffect(() => {
@@ -68,21 +42,7 @@ export default function Transictions(props) {
         </TableHead>
         <TableBody>
           {Transactions.map((tr) => (
-            <StyledTableRow key={tr.transaction_id}>
-              <StyledTableCell component="th" scope="row">
-                {tr.transaction_vendor}
-              </StyledTableCell>
-              <StyledTableCell align="left">{tr.transiction_amount}</StyledTableCell>
-              <StyledTableCell align="left">{tr.category_name}</StyledTableCell>
-             <StyledTableCell>
-              <Box textAlign='left'>
-                <Button justify="left" variant="contained" color="error">
-                   delete
-                </Button>
-              </Box>
-            </StyledTableCell>
-             
-            </StyledTableRow>
+            <Transaction transaction={tr}/>
           ))}
         </TableBody>
       </Table>
